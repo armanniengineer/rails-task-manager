88018558a0bd69cf72e37d4cc777e60c44eb42e4
Armanni AC <armanniatac@gmail.com>
2017-08-08 17:03:24 +0800

    ugly task manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(content: "your mum", date: "12th August 2015")
+Task.create(content: "your dad", date: "4th December 2015")
+Task.create(content: "the one", date: "2nd Feburary 2016")
+Task.create(content: "you never had", date: "14th March 2016")
+Task.create(content: "your sister", date: "7th May 2016")
+Task.create(content: "your brother", date: "29th July 2016")
+Task.create(content: "the one that fucked your mother", date: "1st September 2016")
